790644dc0d99d32026beb9eb00cdb31d0950a429
Rado Buransky <radoslav.buransky@avitech.aero>
2015-11-16 09:25:37 +0100

    Learning node.js...

--- src/d3sheet.js
+++ src/d3sheet.js
@@ -1,3 +1,32 @@
-module.exports = function() {
-  return 'd3sheet';
-}
+var init = require("./init");
+
+!function() {
+    checkRequirements();
+
+    var d3sheet = {
+        ver: "1.0.0"
+    };
+
+    module.exports = d3sheet;
+
+    // Initialize D3 sheet
+    d3sheet.init = function(containerId) {
+        if (containerId == null) {
+            // Use default
+            d3sheet.containerId = "d3sheet-container";
+        }
+        else {
+            d3sheet.containerId = containerId;
+        }
+
+        d3.select("#d3sheet-container")
+            .append("svg:svg");
+    }
+
+    function checkRequirements() {
+        if (typeof d3 === "undefined")
+            throw new Error("D3 library not found!");
+        if (typeof $ === "undefined")
+            throw new Error("jQuery not found!");
+    }
+}();
